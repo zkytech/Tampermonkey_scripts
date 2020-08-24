584edcbfaa2cc81ff0960a5f4e433ee706931498
zkytech <zhangkunyuan@hotmail.com>
2020-08-24 21:47:04 +0800

    适配av

--- bilibili-collection-enhance.user.js
+++ bilibili-collection-enhance.user.js
@@ -1,19 +1,115 @@
 // ==UserScript==
 // @name         Bilibili合集观看进度
 // @namespace    https://github.com/zkytech/Tampermonkey_scripts
-// @version      0.6.3
+// @version      0.6.5
 // @description  显示合集整体观看进度，方便掌控学习进度，合理安排学习时间。
 // @author       zkytech
 // @include      *://www.bilibili.com/video/BV*
+// @include      *://www.bilibili.com/video/av*?p=*
 // @grant        none
 // ==/UserScript==
 
 (function () {
     'use strict';
+    var dict = [
+        'f', 'Z', 'o', 'd', 'R', '9', 'X', 'Q', 'D', 'S', 'U', 'm', '2', '1', 'y', 'C', 'k', 'r', '6', 'z', 'B', 'q', 'i', 'v', 'e', 'Y', 'a', 'h', '8',
+        'b', 't', '4', 'x', 's', 'W', 'p', 'H', 'n', 'J', 'E', '7', 'j', 'L', '5', 'V', 'G', '3', 'g', 'u', 'M', 'T', 'K', 'N', 'P', 'A', 'w', 'c', 'F'
+    ]
+
+    var cn_n = 177451812;
+    var cn_a = '100618342136696320';
+
+    function BigAdd(a, b) {
+        var min_str = a.length < b.length ? a.split("").reverse() : b.split("").reverse();
+        var max_str = a.length >= b.length ? a.split("").reverse() : b.split("").reverse();
+        var quotient = 0,
+            remainder = 0;
+        var resutl = [];
+        var temp = 0;
+        for (var i = 0; i < min_str.length; i++) {
+            temp = parseInt(min_str[i]) + parseInt(max_str[i]) + quotient;
+            quotient = parseInt(temp / 10); //进位
+            remainder = temp % 10; //余数
+            resutl.push(remainder);
+        }
+        for (i; i < max_str.length; i++) {
+            temp = parseInt(max_str[i]) + quotient;
+            quotient = temp / 10; //进位
+            remainder = temp % 10; //余数
+            resutl.push(remainder);
+            if (temp < 10 && i < max_str.length - 1) {
+                return max_str.slice(i + 1).reverse().join("") + resutl.slice().reverse().join("");
+            }
+        }
+    }
+
+
+    function BigDiv(a, b) {
+        var alen = a.length,
+            blen = b.length;
+        var quotient = 0,
+            remainder = 0;
+        var result = [],
+            temp = 0;
+        for (var i = 0; i < alen; i++) {
+            temp = remainder * 10 + parseInt(a[i]);
+            if (temp < b) {
+                remainder = temp;
+                result.push(0);
+            } else {
+                quotient = parseInt(temp / b);
+                remainder = temp % b;
+                result.push(quotient);
+            }
+
+        }
+        return [result.join("").replace(/\b(0+)/gi, ""), remainder];
+    }
+
+    /**
+     * av 转 bv
+     * @param {*} av 
+     */
+    function toBv(av) {
+        var p = BigAdd((av ^ cn_n).toString(), cn_a);
+        var res = [];
+        for (var i = 0; i < 10; i++) {
+            res.push(
+                BigDiv(
+                    BigDiv(
+                        p, Math.pow(58, i).toString()
+                    )[0],
+                    '58'
+                )[1]
+            );
+        }
+        var mix = [];
+        var d = [6, 2, 4, 8, 5, 9, 3, 7, 1, 0];
+        var temp = res.map(function (v) {
+            return dict[v];
+        });
+        for (var i in d) {
+            mix.push(temp[d[i]]);
+        }
+        return mix.join('');
+    }
+
+
     let total_duration = 0
     // 首先必须是BV链接才执行后面的程序
-    if (window.location.pathname.includes("/video/BV")) {
-        const bvid = window.location.pathname.split("/")[2].slice(2)
+
+    if (window.location.pathname.includes("/video/BV") || (window.location.pathname.includes("/video/av") && window.location.search.includes("p="))) {
+        let bvid = ""
+        if (window.location.pathname.includes("/video/BV")) {
+            bvid = window.location.pathname.split("/")[2].slice(2)
+        } else {
+            // av转bv
+            const avid = window.location.pathname.split("/")[2].slice(2)
+            console.log(avid)
+            bvid = toBv(avid + "")
+        }
+        console.log("bvid=", bvid)
+
         // 请求API获取合集信息
         fetch(`https://api.bilibili.com/x/web-interface/view?bvid=${bvid}`).then(res => res.json()).then(res => {
             const data = res.data
@@ -86,14 +182,14 @@
                              */
                             function update_mark() {
 
-                                const target_p = get_target_p(bvid,data) // 观看目标的终点分P的序列号
+                                const target_p = get_target_p(bvid, data) // 观看目标的终点分P的序列号
 
                                 const page_list = [...document.querySelectorAll(".list-box > li > a")]
                                 const target_page = page_list[target_p - 1]
-                                const progress_p = page_list.slice(current_p, target_p-1)
+                                const progress_p = page_list.slice(current_p, target_p - 1)
                                 // 首先清除所有标记
-                                page_list.forEach((v,i) => {
-                                    if (i < current_p || i >= target_p){
+                                page_list.forEach((v, i) => {
+                                    if (i < current_p || i >= target_p) {
                                         v.classList.remove("zky_target_p")
                                         v.classList.remove("zky_progress_p")
                                         v.classList.remove("zky_p_mark")
@@ -110,7 +206,7 @@
                                 // 进度条标记
                                 if (target_p === current_p && !document.querySelector("#zky_controller_progress_mark")) {
                                     const cp_mark = document.createElement("div")
-                                    cp_mark.setAttribute("id","zky_controller_progress_mark")
+                                    cp_mark.setAttribute("id", "zky_controller_progress_mark")
                                     cp_mark.setAttribute("style", "background-color:#52C41A;width:2px;height:5px")
                                     document.querySelector(".bui-bar-wrap").appendChild(cp_mark)
                                 }
@@ -144,7 +240,7 @@
              */
             function update_() {
                 try {
-                    const target_p = get_target_p(bvid,data) // 观看目标的终点分P的序列号
+                    const target_p = get_target_p(bvid, data) // 观看目标的终点分P的序列号
                     let show_ext_info = document.querySelector("#bilibiliPlayer").className.includes("screen") // 当前是否处于全屏/网页全屏/宽屏模式
                     document.querySelector(".bilibili-player-video-control").clientWidth > 950 ? show_ext_info = true : show_ext_info = false // 只要播放器宽度超过900 也显示倒计时信息
                     const current_p = get_current_p()
@@ -159,11 +255,11 @@
                         target_distance = target_time - finished_duration
                         show_ext_info && document.querySelector("#zky_target_distance").removeAttribute("style")
                         document.querySelector("#zky_target_distance > span").textContent = format_seconds(target_distance)
-                    } 
+                    }
                     // 如果没有设定目标，就隐藏进度信息
                     else if (!target_time) {
                         document.querySelector("#zky_target_distance").setAttribute("style", "display:none")
-                    } 
+                    }
                     // 设定了目标且目标已经完成，显示“已完成目标”
                     else {
                         show_ext_info && document.querySelector("#zky_target_distance").removeAttribute("style")
@@ -171,7 +267,7 @@
                     }
                     show_ext_info ? document.querySelector("#zky_finished_percent").removeAttribute("style") : (document.querySelector("#zky_target_distance").setAttribute("style", "display:none") || document.querySelector("#zky_finished_percent").setAttribute("style", "display:none"))
                     // 设定目标分P的进度条提示
-                    if(target_p === current_p){
+                    if (target_p === current_p) {
                         const ctrl_width = document.querySelector(".bui-bar-wrap").offsetWidth
                         document.querySelector("#zky_controller_progress_mark").style.transform = `translateX(${ctrl_width * ((target_time - (finished_duration - Number(min_) * 60 - Number(sec_)))/data.pages[current_p-1].duration )}px)`
                     }
@@ -201,10 +297,10 @@
 
     /**
      * 获取观看目标的分P序列号
-     * @param {string} bvid 
-     * @param {any[]} data 
+     * @param {string} bvid
+     * @param {any[]} data
      */
-    function get_target_p(bvid,data) {
+    function get_target_p(bvid, data) {
         let target_p = 0
         let __temp_duration = 0
         localStorage[`zky_target_${bvid}`] && data.pages.some((v, i) => {
